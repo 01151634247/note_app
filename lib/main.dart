5ea078d78e8703300183b1da0056070653090939
ayman <aymanmohamed1142001@gmail.com>
2026-09-16 08:55:18 +0300

    create homepage

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:note_app/pages/home_page.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const NoteApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class NoteApp extends StatelessWidget {
+  const NoteApp({super.key});
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      home:HomePage(),
      
     );
   }
