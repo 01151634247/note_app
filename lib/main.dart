909d4035ba566c1ba2adcda9c68ebddc543a48f7
ayman <aymanmohamed1142001@gmail.com>
2026-09-17 03:05:09 +0300

    provide add notecuibt

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:note_app/constant.dart';
+import 'package:note_app/cubits/cubit/add_note_cubit.dart';
 import 'package:note_app/model/adaptor_model.dart';
 
 import 'package:note_app/pages/home_page.dart';
 
-void main() async{
+void main() async {
   await Hive.initFlutter();
- await Hive.openBox(kNotesBooks);
- Hive.registerAdapter(AdaptorModel());
+  await Hive.openBox(kNotesBooks);
+  Hive.registerAdapter(AdaptorModel());
 
   runApp(const NoteApp());
 }
@@ -19,14 +21,19 @@ class NoteApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        brightness: Brightness.dark,
-        fontFamily:'Poppins',
+    return  MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => AddNoteCubit(),
+          
+        ),
+      ],
+      child: MaterialApp(
+          debugShowCheckedModeBanner: false,
+          theme: ThemeData(brightness: Brightness.dark, fontFamily: 'Poppins'),
+          home: const HomePage(),
+        
       ),
-      home:const HomePage(),
-     
     );
   }
 }
