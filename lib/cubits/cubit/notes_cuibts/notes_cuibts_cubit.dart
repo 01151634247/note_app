1df93d7dd235d24e3c71fcd740adc2c9e0258010
ayman <aymanmohamed1142001@gmail.com>
2026-09-18 03:44:04 +0300

    create fetch note method in cubits

--- lib/cubits/cubit/notes_cuibts/notes_cuibts_cubit.dart
+++ lib/cubits/cubit/notes_cuibts/notes_cuibts_cubit.dart
@@ -1,13 +1,27 @@
 
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hive/hive.dart';
+import 'package:note_app/constant.dart';
 import 'package:note_app/cubits/cubit/notes_cuibts/notes_cuibts_state.dart';
+import 'package:note_app/model/note_model.dart';
 
 
 class NotesCuibtsCubit extends Cubit<NotesCuibtsState> {
   NotesCuibtsCubit() : super(NotesCuibtsInitial());
-  
 
 
+   fetchAllNote(){
+    try {
+      var notesBox=Hive.box<NoteModel>(kNotesBooks);
+        List<NoteModel> note=notesBox.values.toList();
+      emit(NotesCuibtsSuccess(note));
+    } catch (e) {
+      emit(NotesCuibtsFaliure(e.toString()));
+      
+    }
+
+
+   }
 
 
 
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:note_app/cubits/cubit/notes_cuibts/notes_cuibts_cubit.dart';
 import 'package:note_app/widgets/add_note_button_shett.dart';
 import 'package:note_app/widgets/home_page_body.dart';
 
@@ -7,22 +9,26 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      floatingActionButton: FloatingActionButton(onPressed: (){
-        showModalBottomSheet(
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(36),
-          ),
-          context: context, builder:(context){
-          return addNoteButtonSheet();
-        });
-      },
-      backgroundColor: Colors.blueAccent,
-      child: Icon(Icons.add),
+    return BlocProvider(
+      create: (context) => NotesCuibtsCubit(),
+      child: Scaffold(
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            showModalBottomSheet(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(36),
+              ),
+              context: context,
+              builder: (context) {
+                return addNoteButtonSheet();
+              },
+            );
+          },
+          backgroundColor: Colors.blueAccent,
+          child: Icon(Icons.add),
+        ),
+        body: HomePageBody(),
       ),
-      body:HomePageBody() ,
     );
-    
   }
 }
-
